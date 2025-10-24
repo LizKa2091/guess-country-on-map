6f0797b609d0eb1799253c557258e5d10f8795a9
Liza <lizka4231@gmail.com>
2025-10-24 15:22:45 +0300

    feat(WorldMap): add hovering country on map

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { useState, type FC } from 'react';
 import * as d3 from 'd3-geo';
 import { feature } from 'topojson-client';
 import worldData from '../assets/world-110m.json';
@@ -7,6 +7,8 @@ import type { ICountryItem } from '../types/countryTypes';
 import styles from './WordMap.module.scss';
 
 const WorldMap: FC = () => {
+   const [hovered, setHovered] = useState<string | null>(null);
+
    const projection = d3.geoMercator().scale(100).translate([400, 250]);
    const pathGenerator = d3.geoPath().projection(projection);
 
@@ -14,15 +16,22 @@ const WorldMap: FC = () => {
 
    return (
       <svg className={styles.svg}>
-         {countries.map((data: ICountryItem) => (
-            <path
-               key={data.id}
-               d={pathGenerator(data)}
-               fill='#ddd'
-               store='#333'
-               onClick={() => console.log(data.properties.name || data.id)}
-            />
-         ))}
+         {countries.map((data: ICountryItem) => {
+            const name = data.properties.name || data.id;
+            const isHovered = hovered === name;
+
+            return (
+               <path
+                  key={data.id}
+                  d={pathGenerator(data)}
+                  fill={isHovered ? "#ffcc00" : "#ddd"}
+                  stroke='#333'
+                  onMouseEnter={() => setHovered(name)}
+                  onMouseLeave={() => setHovered(null)}
+                  onClick={() => console.log(data.properties.name || data.id)}
+               />
+            )
+         })}
       </svg>
    )
 }
