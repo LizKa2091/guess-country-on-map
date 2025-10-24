3f92a9667a3a50d932d9c288449f0a85b222d0bc
Liza <lizka4231@gmail.com>
2025-10-24 14:10:52 +0300

    styles: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { type FC } from "react"
+import WorldMap from "./components/WorldMap";
+import './styles/global.scss';
 
 const App: FC = () => {
 
    return (
       <>
-         guess country on map
+         <h1>guess country on map</h1>
+         <WorldMap />
       </>
    )
 }
