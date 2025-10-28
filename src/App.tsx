f792d932606ba24f550c05ab2f14fd1f0082a5f8
Liza <lizka4231@gmail.com>
2025-10-28 15:48:35 +0300

    feat(CountriesList): add countries list layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useMemo, type FC } from "react"
-import WorldMap from "./components/WorldMap/WorldMap";
 import { feature } from 'topojson-client';
 import worldData from './assets/world-110m.json';
+import WorldMap from "./components/WorldMap/WorldMap";
+import CountriesList from "./components/CountriesList/CountriesList";
+import type { ICountryItem } from "./types/countryTypes";
 
 import './styles/global.scss';
 
 const App: FC = () => {
-   const countries = useMemo(() => 
+   const countries: ICountryItem[] = useMemo(() => 
       feature(worldData, worldData.objects.countries).features, 
    []);
 
@@ -14,6 +16,7 @@ const App: FC = () => {
       <div className='app-container'>
          <h1>guess country on map</h1>
          <WorldMap countries={countries} />
+         <CountriesList countries={countries} />
       </div>
    )
 }
